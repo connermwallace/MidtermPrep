483a1c5e0e759382712fcb7e3dc80e409fad4f69
connermwallace <connerw@uw.edu>
2018-10-31 18:55:36 -0700

    Added new problem

---
+++ findMatching.java
@@ -0,0 +1,43 @@
+/*
+Write a static method findMatching which takes two strings
+as parameters and returns a string of the characters that
+are a match in both strings, and '.' otherwise. Characters
+are considered a match when equal characters appear at the
+exact same location in both strings. Characters in the given
+strings which don't match should be represented with a '.' 
+character in the resulting string.
+*/ 
+
+public class findMatching {
+
+   public static void main(String[] args) {
+      System.out.println(findMatching("bunK", "punKed"));
+   }
+   
+   public static String findMatching(String a, String b) {
+      String result = "";
+      
+      if(a.length() > b.length()) {
+         for (int i = 0; i < a.length(); i++) {
+            if (i > b.length() -1 || a.charAt(i) != b.charAt(i)) {
+               result += '.';
+            } else if (a.charAt(i) == b.charAt(i)) {
+               result += a.charAt(i);
+            }
+         }
+      } else {
+         for (int i = 0; i < b.length(); i++) {
+            if (i > a.length() -1 || a.charAt(i) != b.charAt(i)) {
+               result += '.';
+            } else if (a.charAt(i) == b.charAt(i)) {
+               result += a.charAt(i);
+            }
+         }
+      }
+      return result;
+   }
+}
+   
+         
+      
+            
